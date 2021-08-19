53208034a87bfb886458d79e810dfdf6cf9db000
Ziqi Wang <ziqi.wang39@gmail.com>
2021-08-19 15:53:46 +1000

    Fetch API with search users

--- src/App.js
+++ src/App.js
@@ -17,12 +17,19 @@ class App extends Component{
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRETS}`)
     this.setState({users:res.data, loading:false})
   }
+  
+searchUsers = async (text)=>{
+    this.setState({loading:true})
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRETS}`)
+    this.setState({users:res.data.items, loading:false})
+  }
+
   render(){
     return(
         <div className='App'>
            <Navbar title='Github finder' icon="fab fa-github"/>
            <div className="container">
-                 <Search />
+                 <Search  searchUsers={this.searchUsers}/>
                 <Users loading={this.state.loading} users={this.state.users}/>
            </div>   
         </div>
