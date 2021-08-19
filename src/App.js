ab00053bdd023096373c7107d9d6bfb551613362
Ziqi Wang <ziqi.wang39@gmail.com>
2021-08-19 15:15:13 +1000

    Add seach bar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 
 
@@ -14,7 +15,6 @@ class App extends Component{
   async componentDidMount(){
     this.setState({loading:true})
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRETS}`)
-    console.log(res.data)
     this.setState({users:res.data, loading:false})
   }
   render(){
@@ -22,6 +22,7 @@ class App extends Component{
         <div className='App'>
            <Navbar title='Github finder' icon="fab fa-github"/>
            <div className="container">
+                 <Search />
                 <Users loading={this.state.loading} users={this.state.users}/>
            </div>   
         </div>
